5ed73132cc3f500c15734f046e90a636d419d61c
Hignem <81192237+Hignem@users.noreply.github.com>
2024-06-07 16:53:07 +0200

    finito

--- befit/src/app/hooks/recipemainPost.ts
+++ befit/src/app/hooks/recipemainPost.ts
@@ -12,103 +12,14 @@ export interface RecipeData {
   image: File | null;
 }
 
-// export async function submitRecipe(data: RecipeData) {
-//   // const formData = new FormData();
-//   // formData.append("nazwa", data.nazwa);
-//   // formData.append("przygotowanie", data.przygotowanie);
-
-//   // data.skladniki.forEach((ingredient) => {
-//   //   formData.append("skladnik", ingredient.skladnik);
-//   // });
-
-//   // if (data.image) {
-//   //   formData.append("image", data.image);
-//   // }
-
-//   try {
-//     // console.log("Wysyłane dane:", data);
-//     // const formData = new FormData();
-//     // formData.append("file", data.image || new File([], "plik"));
-
-//     const formData: RecipeData = {
-//       nazwa: data.nazwa,
-//       skladniki: data.skladniki,
-//       przygotowanie: data.przygotowanie,
-//       image: data.image, // Pobieramy plik z formData
-//     };
-//     const response: AxiosResponse<void> = await appAPI.post(
-//       `/api/przepisy`,
-//       formData,
-//       {
-//         withCredentials: true,
-//       }
-//     );
-//   } catch (error: any) {
-//     console.error("Wystąpił błąd podczas przesyłania przepisu", error.message);
-//     console.error("Szczegóły błędu:", error);
-//     return `Wystąpił błąd podczas przesyłania przepisu: ${error.message}`;
-//   }
-// }
-
-// import { appAPI } from "@/utils/appAPI";
-// import { AxiosResponse } from "axios";
-
-// export interface Ingredient {
-//   skladnik: string;
-// }
-
-// export interface RecipeData {
-//   nazwa: string;
-//   skladniki: Ingredient[];
-//   przygotowanie: string;
-//   image: File | null;
-// }
-
-// export async function submitRecipe(data: RecipeData): Promise<string> {
-//   const formData = new FormData();
-//   formData.append("nazwa", data.nazwa);
-//   formData.append("przygotowanie", data.przygotowanie);
-
-//   data.skladniki.forEach((ingredient, index) => {
-//     formData.append(`skladniki[${index}][skladnik]`, ingredient.skladnik);
-//   });
-
-//   if (data.image) {
-//     formData.append("image", data.image);
-//   }
-
-//   try {
-//     console.log("Wysyłane dane:", Array.from(formData.entries()));
-
-//     const response: AxiosResponse<string> = await appAPI.post(
-//       `/api/przepisy`,
-//       formData,
-//       {
-//         headers: {
-//           "Content-Type": "multipart/form-data",
-//         },
-//         withCredentials: true,
-//       }
-//     );
-
-//     return response.data;
-//   } catch (error: any) {
-//     console.error("Wystąpił błąd podczas przesyłania przepisu", error.message);
-//     console.error("Szczegóły błędu:", error.response.data);
-//     return `Wystąpił błąd podczas przesyłania przepisu: ${error.message}`;
-//   }
-// }
 export async function submitRecipe(recipeData: RecipeData): Promise<string> {
   try {
+    const { image, ...recipeWithoutImage } = recipeData;
     const formData = new FormData();
-    formData.append("nazwa", recipeData.nazwa);
-    // formData.append("skladniki", JSON.stringify(recipeData.skladniki));
-    recipeData.skladniki.forEach((ingredient, index) => {
-      formData.append(`skladniki[${index}].skladnik`, ingredient.skladnik);
-    });
-    formData.append("przygotowanie", recipeData.przygotowanie);
-    if (recipeData.image) {
-      formData.append("image", recipeData.image);
+
+    formData.append("przepis", JSON.stringify(recipeWithoutImage));
+    if (image) {
+      formData.append("image", image);
     }
     console.log(formData);
 
@@ -119,6 +30,7 @@ export async function submitRecipe(recipeData: RecipeData): Promise<string> {
         headers: {
           "Content-Type": "multipart/form-data",
         },
+        withCredentials: true,
       }
     );
 
--- befit/src/app/panel/przepisy/page.tsx
+++ befit/src/app/panel/przepisy/page.tsx
@@ -6,12 +6,16 @@ import {
 } from "@/app/hooks/recipemainPost";
 import { MyButton } from "@/components/common/ButtonAdminPanel";
 import React, { useState } from "react";
-
+import { FiAlertCircle, FiLoader } from "react-icons/fi";
+import { GrStatusGood } from "react-icons/gr";
 const RecipeForm = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [recipeName, setRecipeName] = useState("");
   const [ingredients, setIngredients] = useState<string>("");
   const [instructions, setInstructions] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<boolean>(false);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files && event.target.files[0];
@@ -26,6 +30,23 @@ const RecipeForm = () => {
     if (name === "recipeName") setRecipeName(value);
     if (name === "ingredients") setIngredients(value);
     if (name === "instructions") setInstructions(value);
+  };
+
+  const handleReset = () => {
+    setSelectedFile(null);
+    setRecipeName("");
+    setIngredients("");
+    setInstructions("");
+    setError(null);
+    setSuccess(false);
+    console.log("Stany po resetowaniu:", {
+      selectedFile,
+      recipeName,
+      ingredients,
+      instructions,
+      error,
+      success,
+    });
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -45,20 +66,26 @@ const RecipeForm = () => {
 
     console.log("Wysłane dane:", recipeData);
 
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+
     const result = await submitRecipe(recipeData);
 
     if (result === "Przepis został dodany pomyślnie") {
       console.log("Przepis dodany pomyślnie!");
+      handleReset();
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+      }, 5000);
     } else {
       console.error(result);
+      setError(result);
     }
+    setLoading(false);
   };
-  const handleReset = () => {
-    setSelectedFile(null);
-    setRecipeName("");
-    setIngredients("");
-    setInstructions("");
-  };
+
   return (
     <div className=" flex flex-col bg-gradient-to-b from-white from-10% to-[#b8ccc4] to-90% text-black font-abhaya px-28">
       <form onSubmit={handleSubmit}>
@@ -77,8 +104,11 @@ const RecipeForm = () => {
                 />
               </div>
             ) : (
-              <div className="w-72 h-80 bg-ziel1 m-4 border-2 border-black/40 border-dotted flex justify-center items-center">
+              <div className="w-72 h-80 bg-ziel1 m-4 border-2 border-black/40 border-dotted flex justify-center items-center flex-col">
                 <h2 className="text-lg">Brak wybranego zdjęcia</h2>
+                <h2 className="text-base">
+                  (zalecany rozmiar: 900 x 650 pikseli)
+                </h2>
               </div>
             )}
             <div className="relative mt-2">
@@ -105,9 +135,10 @@ const RecipeForm = () => {
               placeholder="Dodaj nazwę przepisu"
             />
             <div className="bg-white/70 p-4">
-              <label className="block text-black text-2xl mb-2">
-                Składniki:
-              </label>
+              <label className="block text-black text-2xl ">Składniki:</label>
+              <p className="mb-2 text-sm">
+                (poszczególne składniki, należy oddzielić przecinkiem)
+              </p>
               <textarea
                 value={ingredients}
                 onChange={(e) => setIngredients(e.target.value)}
@@ -126,11 +157,30 @@ const RecipeForm = () => {
               ></textarea>
             </div>
             <div className="flex flex-row justify-end gap-5 mt-2">
+              {error && error !== null && (
+                <p className="text-red-500 flex justify-center items-center text-xl font-bold gap-3">
+                  <FiAlertCircle className="text-3xl" />
+                  Wystąpił problem z przesłaniem przepisu!
+                </p>
+              )}
+              {success && (
+                <p className="text-ziel2 flex justify-center items-center text-xl font-bold gap-3">
+                  <GrStatusGood className="text-3xl" />
+                  Przepis dodany pomyślnie!
+                </p>
+              )}
+              {loading && (
+                <p className="text-ziel2 flex justify-center items-center text-xl font-bold gap-3">
+                  <FiLoader className="text-3xl" />
+                  Wysyłanie...
+                </p>
+              )}
               <MyButton className="px-8 py-2 text-xl my-5" type="submit">
                 <p className="">Zapisz</p>
               </MyButton>
               <MyButton
                 className="px-8 py-2 font-medium text-xl my-5"
+                type="button"
                 onClick={handleReset}
               >
                 <p className="">Anuluj</p>
